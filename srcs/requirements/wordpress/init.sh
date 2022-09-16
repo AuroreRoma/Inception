22b663e24a244066716f7bddd024a0b4ecbcd6c7
Aurore Roma <aroma@student.42.fr>
2022-09-16 22:36:00 +0200

    .

--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -1,2 +1,14 @@
-echo "Hello world !"
+echo 'wordpress containers starting'
+if [[ ! -f /var/www/wordpress/index.php ]];
+then
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzvf latest.tar.gz -C /var/www/
+	rm -rf latest.tar.gz
+	echo 'wordpress successfully downloaded and installed !'
+else
+	echo 'wordpress already installed !'
+fi
+
+
+echo 'starting php-fpm8'
 exec /usr/sbin/php-fpm8 -F
