7923c617e0a397f8d5af2c3e10155d8618e73751
Aurore Roma <aroma@student.42.fr>
2022-09-19 22:18:58 +0200

    less go

--- Makefile
+++ Makefile
@@ -10,4 +10,12 @@ down :
 	docker-compose -f srcs/docker-compose.yml down
 
 clean :
+	docker-compose -f srcs/docker-compose.yml down
 	docker-compose -f srcs/docker-compose.yml rm
+
+clear :
+	-@docker stop $(docker ps -qa) 2>/dev/null
+	-@docker rm $(docker ps -qa) 2>/dev/null
+	-@docker rmi -f $(docker images -qa) 2>/dev/null
+	-@docker volume rm $(docker volume ls -q) 2>/dev/null
+	-@docker network rm $(docker network ls -q) 2>/dev/null
--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,17 +4,20 @@ addgroup mysql mysql
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
+chmod -R 777 /run/mysqld
 
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
-#/usr/share/mariadb/mysql.server start
+/usr/share/mariadb/mysql.server start
+
 #mysql_secure_installation
-#/usr/share/mariadb/mysql.server stop
 
-#echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root --skip-password
+echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root --skip-password
 
-#echo "GRANT ALL ON wordpress.* TO 'aroma'@'localhost' IDENTIFIED BY 'tarte42';" | mysql -u root --skip-password
+echo "GRANT ALL ON wordpress.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';" | mysql -u root --skip-password
 
-#echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
-mysqld --user=mysql --bind-address=0.0.0.0
+/usr/share/mariadb/mysql.server stop
+
+mysqld --user=mysql --skip-networking=off --bind-address=0.0.0.0
