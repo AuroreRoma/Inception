e8fbacf6324c9de26bb7c8d54b254dedf7c16351
Loutre <loutre@courrier.dev>
2022-09-19 22:18:58 +0200

    less go

--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -1,10 +1,17 @@
 echo 'wordpress containers starting'
-if [[ ! -f /var/www/wordpress/index.php ]];
+if [[ ! -f /var/www/wordpress/wp-config.php ]];
 then
 	wget https://wordpress.org/latest.tar.gz
 	tar -xzvf latest.tar.gz -C /var/www/
 	rm -rf latest.tar.gz
 	echo 'wordpress successfully downloaded and installed !'
+	cd /var/www/wordpress
+
+	sed -i "s/username_here/$DB_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$DB_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
+	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
+	mv wp-config-sample.php wp-config.php
 else
 	echo 'wordpress already installed !'
 fi
---
+++ test.sh
@@ -0,0 +1,6 @@
+if [[ ! -d tmp ]]; then
+	mkdir tmp;
+	echo 'tmp created'
+else
+	echo 'tmp already exist'
+fi
