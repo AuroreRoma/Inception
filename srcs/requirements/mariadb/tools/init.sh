381b1191dc2e2efe087e29107f4becea8df99d50
Aurore Roma <aroma@student.42.fr>
2022-09-20 00:28:15 +0200

    .

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -8,16 +8,21 @@ chmod -R 777 /run/mysqld
 
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
-/usr/share/mariadb/mysql.server start
+if [ -d "/var/lib/mysql/$DB_NAME" ]
+then
+	echo "$DB_NAME database already exists. Continuing"
+else
 
-#mysql_secure_installation
+	/usr/share/mariadb/mysql.server start
 
-echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root --skip-password
+	echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root --skip-password
 
-echo "GRANT ALL ON wordpress.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql -u root --skip-password
+	echo "GRANT ALL ON wordpress.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql -u root --skip-password
 
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+	echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
-/usr/share/mariadb/mysql.server stop
+	/usr/share/mariadb/mysql.server stop
+
+fi
 
 mysqld --user=mysql --skip-networking=off --bind-address=0.0.0.0
--- test.sh
+++
@@ -1,6 +0,0 @@
-if [[ ! -d tmp ]]; then
-	mkdir tmp;
-	echo 'tmp created'
-else
-	echo 'tmp already exist'
-fi
